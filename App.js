b4ce87db9fdedec193ebb9a654e14759f42caf23
Sumanth_Varma <sumanthvarma.g07@gmail.com>
2021-05-06 14:33:21 +0530

    Basic TO-DO app

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import Header from './components/header';
+import TaskItem from './components/taskItem';
+import AddTask from './components/addTask';
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+    { text: 'milk', key: '1'},
+    { text: 'oranges', key: '2'},
+    { text: 'bread', key: '3'},
+  ]);
+  
+  const pressHandler = (key) => {
+    setTasks(prevTasks => {
+      return prevTasks.filter(task => task.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+
+    if (text.length > 2) {
+      setTasks((prevTasks) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTasks
+        ];
+      });
+    } else {
+      Alert.alert('OOPS!', 'Tasks Must be over 2 characters long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>TEST APP!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <AddTask submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={tasks}
+            renderItem={({ item }) => (
+              <TaskItem item={item} pressHandler={pressHandler} />
+            )}
+           />
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,7 +55,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20
+  }
 });
